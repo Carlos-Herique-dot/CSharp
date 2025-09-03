c33b87cbc8dd2c2a8b48e1d3cfa9357b96c0d974
Carlos Henrique <carlos.herique.dot@gmail.com>
2025-09-03 10:04:24 -0300

    TestesFile
    Aprendendo a manipular arquivos com File

--- ManipulaçãoDeArquivos/Program.cs
+++ ManipulaçãoDeArquivos/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string filePath = @"C:\Users\Carlos\Desktop\test.txt";
+
+File.Delete(filePath);
+// using var file = File.CreateText(filePath);
+// file.WriteLine("Hello, World! 1");
+
+// file.Close();
+
+// string conteudo = File.ReadAllText(filePath);
+// Console.WriteLine(conteudo);
+
+// try
+// {
+//     using var file = File.AppendText(filePath);
+//     file.WriteLine("Hello, World!");
+//     Console.WriteLine("Texto adicionado com sucesso.");
+//     file.Close();
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+//     throw;
+// }
