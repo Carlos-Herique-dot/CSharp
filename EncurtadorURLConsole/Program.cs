c62db5b51c65ac13b4a0ddc4ffa3e897a4f446de
Carlos Henrique <carlos.herique.dot@gmail.com>
2025-09-08 20:02:34 -0300

    Inicio do Encurtador
    Aqui apenas inicia os arquivos

---
+++ EncurtadorURLConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Encurtador de URL\n");
+
+Console.Write("Digite a URL que deseja encurtar: ");
+string? urlLonga = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(urlLonga))
+{
+    if (IsValidUrl(urlLonga))
+    {
+       
+    }
+}
+else
+{
+    Console.WriteLine("URL inválida. Por favor, tente novamente.");
+}
+
+static bool IsValidUrl(string url)
+{
+    return Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)
+    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+}
